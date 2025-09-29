8ec9e2e61c7257b228b556114c36c6f499461762
Cristian Vargas <cristian@dosiscl.com>
2025-09-29 15:14:34 -0300

    Instalación PrimeNG version 20

--- angular-test/src/app/app.ts
+++ angular-test/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChartComponent } from './chart-component/chart-component';
+import { HeaderComponent } from './header-component/header-component';
+import { SearchBarComponent } from './search-bar-component/search-bar-component';
+import { TabComponent } from './tab-component/tab-component';
+import { SummaryComponent } from './summary-component/summary-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChartComponent,HeaderComponent,SearchBarComponent,TabComponent,SummaryComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
